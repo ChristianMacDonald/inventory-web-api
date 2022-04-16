1589641863f3bbffe7a70f8143a7c6e24571b92b
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2022-04-16 02:23:49 -0400

    Add usersRouter to server at /users endpoint

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const usersRouter = require('./routers/usersRouter');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+app.use('/users', usersRouter);
 
 module.exports = app;
