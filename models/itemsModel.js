317c706ced2b90b71f27462707436cbc7bc009b6
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2022-04-20 23:33:41 -0400

    Create updateItemByDPCI and removeItemByDPCI functions in itemsModel

--- models/itemsModel.js
+++ models/itemsModel.js
@@ -20,10 +20,20 @@ function createItem(item) {
   return qb('items').insert(item).returning(['department_id', 'class_id', 'id']);
 }
 
+function updateItemByDPCI(department_id, class_id, id, changes) {
+  return qb('items').where({ department_id, class_id, id }).update(changes);
+}
+
+function removeItemByDPCI(department_id, class_id, id) {
+  return qb('items').where({ department_id, class_id, id }).del();
+}
+
 module.exports = {
   getAll,
   getItemByDPCI,
   getItemsByDepartment,
   getItemsByDepartmentAndClass,
-  createItem
+  createItem,
+  updateItemByDPCI,
+  removeItemByDPCI
 };
