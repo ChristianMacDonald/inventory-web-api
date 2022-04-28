431de4a88343ce1311366642bd4f4ade3cebdc42
Christian MacDonald <chrismacd412@gmail.com>
2022-04-27 23:24:16 -0400

    Use departmentsRouter on /departments endpoint in apiRouter

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const authRouter = require('./authRouter');
 const itemsRouter = require('./itemsRouter');
+const departmentsRouter = require('./departmentsRouter');
 
 router.use('/auth', authRouter);
 router.use('/items', itemsRouter);
+router.use('/departments', departmentsRouter);
 
 module.exports = router;
