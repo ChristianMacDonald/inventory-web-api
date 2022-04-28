772720a91293b87349d3949ed0ba7ad3ca8dbe13
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2022-04-28 00:25:43 -0400

    Add GET request handler to /:id endpoint to allow users to retrieve single department by id

--- routers/departmentsRouter.js
+++ routers/departmentsRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAll } = require('../models/departmentsModel');
+const { getAll, getDepartmentByID } = require('../models/departmentsModel');
 const validateToken = require('../middleware/validateToken');
 
 router.use(validateToken);
@@ -12,6 +12,22 @@ router.get('/', async (req, res) => {
         res.status(200).json(departments);
     } catch {
         res.status(500).json({ errorMessage: 'Could not retrieve departments' });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    let { id } = req.params;
+
+    try {
+        let department = await getDepartmentByID(id);
+        
+        if (department) {
+            res.status(200).json(department);
+        } else {
+            res.status(404).json({ message: `Department with id ${id} not found` });
+        }
+    } catch {
+        res.status(500).json({ errorMessage: 'Could not retrieve department' });
     }
 });
 
