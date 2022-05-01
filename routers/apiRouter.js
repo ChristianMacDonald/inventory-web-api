529a5ffbe546eb2ba36fbebc736cb53ba6eb3a20
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2022-05-01 16:11:25 -0400

    Remove deleted classesRouter from apiRouter

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -4,11 +4,9 @@ const router = express.Router();
 const authRouter = require('./authRouter');
 const itemsRouter = require('./itemsRouter');
 const departmentsRouter = require('./departmentsRouter');
-const classesRouter = require('./classesRouter');
 
 router.use('/auth', authRouter);
 router.use('/items', itemsRouter);
 router.use('/departments', departmentsRouter);
-router.use('/classes', classesRouter);
 
 module.exports = router;
